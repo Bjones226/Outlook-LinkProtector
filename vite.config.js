97dd4d4780e6cec5bd99b07e0d4733195d14fa9c
Pappy <118399290+Bjones226@users.noreply.github.com>
2025-03-27 12:55:41 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
+import { defineConfig } from 'vite';
 
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: {
+    port: 3000
+  }
+});
